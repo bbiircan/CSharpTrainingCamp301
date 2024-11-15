65dbfa99b0da94199e5c1863752a99638b817cf9
bbiircan <bircanbircan1963@gmail.com>
2024-11-15 14:14:20 +0300

    Add project files.

---
+++ CSharpTrainingCamp301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpTrainingCamp301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId {  get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+
+    }
+}
---
+++ CSharpTrainingCamp301.EntityLayer/Concrete/Order.cs
@@ -0,0 +1,7 @@
+﻿namespace CSharpTrainingCamp301.EntityLayer.Concrete
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+    }
+}
